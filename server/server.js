95b950651d0bbb8a6fd59eaabf5ae679ef24250f
AstroLabs Mac Pro <ahmadabugosh@gmail.com>
2017-01-07 17:49:13 +0400

    Added user model

--- server/server.js
+++ server/server.js
@@ -11,46 +11,75 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 var Todo= mongoose.model('Todo', {
 
 	text : {
-		type: String
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
 
 	}, 
 	completed: 
 	{
-		type: Boolean
+		type: Boolean,
+		default: false
 
 	}, 
 	completedAt:
 	{
-		type: Number
+		type: Number,
+		default: null
 
 	}
 
 
 });
 
-var newTodo = new Todo (
+
+//create user model
+var User= mongoose.model('User', {
+
+	email : {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
+
+	}
+
+
+});
+
+// var newTodo = new Todo (
+// {
+// });
+
+
+// newTodo.save().then((doc)=> {
+// console.log('Saved todo',doc);
+// }, (e) => {
+// 	console.log('Unable to save todo');
+// });
+
+// var otherTodo = new Todo (
+// {
+// 	text: 'This is a test',
+// });
+
+// otherTodo.save().then((doc)=> {
+// console.log('Saved todo',JSON.stringify(doc,undefined,2));
+// }, (e) => {
+// 	console.log('Unable to save todo');
+// });
+
+
+var user = new User (
 {
-	text: 'Cook dinner'
+	email: 'ahmadabugosh@gmail.com',
 });
 
-var otherTodo = new Todo (
-{
-	text: 'exercise',
-	completed: true,
-	completedAt: 123
-});
-
-newTodo.save().then((doc)=> {
-console.log('Saved todo',doc);
+user.save().then((doc)=> {
+console.log('User saved',JSON.stringify(doc,undefined,2));
 }, (e) => {
-	console.log('Unable to save todo');
+	console.log('Unable to save user',e);
 });
-
-otherTodo.save().then((doc)=> {
-console.log('Saved todo',JSON.stringify(doc,undefined,2));
-}, (e) => {
-	console.log('Unable to save todo');
-});
-
 
 
