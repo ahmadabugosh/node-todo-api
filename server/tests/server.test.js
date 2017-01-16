778fced9df75ecbc77a35858ebcf9fc6b4b7a4c4
AstroLabs Mac Pro <ahmadabugosh@gmail.com>
2017-01-16 22:45:54 +0400

    Added delete test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -133,3 +133,59 @@ describe('GET /todos', () => {
 	});
 
 });
+
+
+
+
+describe('DELETE /todos/:id', () => {
+ it('should remove a todo', (done) => 
+	{
+
+		var hexID= todos[1]._id.toHexString();
+		request(app)
+		.delete(`/todos/${hexID}`)
+		.expect(200)
+		.expect((res)=> {
+			expect(res.body.todo._id).toBe(hexID);
+		})
+ .end((err,res) => {
+ 	if (err)
+ 	{
+ 		return done(err);
+ 	}
+
+
+				Todo.findById(hexID).then((todos)=> {
+
+					expect(todos).toNotExist(0);
+					done();
+
+
+				}).catch((e)=> done(e));
+ 
+
+ });
+
+	});
+
+  it('should return 404 if todo is not found', (done) => 
+	{
+		var hex= new ObjectID().toHexString();
+		request(app)
+		.delete(`/todos/${hex}`)
+		.expect(404)
+ .end(done);
+		
+
+	});
+
+   it('should return 404 if object is not valid', (done) => 
+	{
+		request(app)
+		.delete(`/todos/123`)
+		.expect(404)
+ .end(done);
+
+	});
+
+});
