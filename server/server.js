e1254b0fa2adf76779ba81f15556a38a5857bb6e
AstroLabs Mac Pro <ahmadabugosh@gmail.com>
2017-01-17 22:26:29 +0400

    Added patch for id updates

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 var express= require('express');
 var bodyParser= require('body-parser');
 
@@ -80,6 +81,32 @@ Todo.findByIdAndRemove(id).then((todo)=> {
 }).catch((e) => {
 	res.status(404).send();
 });
+
+});
+
+app.patch('/todos/:id', (req,res)=> {
+	var id =req.params.id;
+	var body = _.pick(req.body, ['text' , 'completed']);
+
+	if(!ObjectID.isValid(id)){
+	return res.status(404).send();
+}
+if(_.isBoolean(body.completed) && body.completed)
+{
+	body.completedAt = new Date().getTime();
+} else {
+body.completed=false;
+body.completedAt = null;
+}
+Todo.findByIdAndUpdate(id,{$set:body} , {new: true}).then((todo) => {
+if(!todo){
+		return res.status(404).send();
+	}
+	res.send({todo});
+}).catch((e) => {
+	res.status(404).send();
+});
+
 
 });
 
