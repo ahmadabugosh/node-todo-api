29220714a24b64b9e617fe12d8f06b7dac787c75
AstroLabs Mac Pro <ahmadabugosh@gmail.com>
2017-01-19 23:52:21 +0400

    Seperated test and dev env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 mongoose.Promise= global.Promise;
 
 //connect to database locally or heroku
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 //exports mongoose
 
