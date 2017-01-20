1ad22c0cc9f8cc16485cd15f29181f5340593f4d
AstroLabs Mac Pro <ahmadabugosh@gmail.com>
2017-01-20 15:12:20 +0400

    Added post users

--- server/server.js
+++ server/server.js
@@ -112,6 +112,26 @@ if(!todo){
 
 });
 
+app.post('/users', (req,res)=> {
+
+	var body =_.pick(req.body, ['email' , 'password']);
+	var user= new User(body);
+
+	user.save().then((doc)=> {
+		res.send(doc);
+
+	}).catch((e) => {
+
+		res.status(400).send(e);
+
+	})
+
+});
+
+
+
+
+
 
 app.listen(port, () => {
 console.log(`Started on port ${port}`);
