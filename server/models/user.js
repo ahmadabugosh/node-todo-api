1b1abdfe31fa18e46390d05d93b2a5313976850f
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-01-23 23:21:15 +0400

    Added token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,10 @@
+
 const mongoose = require('mongoose');
 const validator = require('validator');
-var User= mongoose.model('User', {
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+var UserSchema = new mongoose.Schema({
 
 	email : {
 		type: String,
@@ -37,7 +41,27 @@ tokens: [{
 
 }]
 
-
 });
+
+UserSchema.methods.toJSON = function () {
+	var user=this;
+	var userObject = user.toObject();
+
+	return _.pick(userObject, ['_id', 'email']);
+
+};
+
+UserSchema.methods.generateAuthToken= function() {
+  var user =this;
+  var access= 'auth';
+  var token= jwt.sign({_id:user._id.toHexString(), access},  'abc123').toString();
+
+  user.tokens.push({access,token});
+  return user.save().then( () => {
+  	return token;
+
+  });
+};
+var User= mongoose.model('User', UserSchema);
 
 module.exports ={User};
--- server/server.js
+++ server/server.js
@@ -117,8 +117,11 @@ app.post('/users', (req,res)=> {
 	var body =_.pick(req.body, ['email' , 'password']);
 	var user= new User(body);
 
-	user.save().then((doc)=> {
-		res.send(doc);
+	user.save().then(()=> {
+		return user.generateAuthToken();
+
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
 
 	}).catch((e) => {
 
