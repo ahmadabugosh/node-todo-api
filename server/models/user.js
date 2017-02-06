1f2cb03a1f7774e6b6bf30dd36b62d938cad4406
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-02-06 22:26:42 +0400

    Added post /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -90,7 +90,37 @@ return User.findOne ({
 
 };
 
-//added user password hashing to schema
+UserSchema.statics.findByCredentials = function(email, password) {
+
+	var User = this;
+	return User.findOne({email}).then((user)=> {
+
+		if(!user)
+		{
+
+			return Promise.reject();
+		}
+
+		return new Promise((resolve,reject) => {
+			bcrypt.compare(password, user.password, (err,res) => {
+
+				if(res)
+				{
+					resolve(user);
+				} else
+				{
+
+					reject();
+				}
+			});
+
+		});
+
+	});
+
+};
+
+//added user password hashing to schema 
 UserSchema.pre('save', function(next) {
 var user=this;
 
